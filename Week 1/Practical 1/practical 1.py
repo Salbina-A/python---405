731621f64961a6c76e05dd60bde486be3159b305
Salbina-A <63582285+Salbina-A@users.noreply.github.com>
2020-04-16 20:19:32 +0400

    Update practical 1.py

--- Week 1/Practical 1/practical 1.py
+++ Week 1/Practical 1/practical 1.py
@@ -2,3 +2,13 @@ def implication3(a, b, c):
 	return bool(a or (b and c))
 
 print(implication3(0, 0, 0))
+
+
+
+def last_digit(N):
+	summ = 0
+	for i in range(1, N+1):
+		summ = summ + (i*i)
+		ld = summ%10
+	return ld
+print(last_digit(950))
